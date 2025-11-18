d0afccfbd3d3aadadfa5fed939f51a935500ecb4
snelusha <hello@snelusha.dev>
2025-11-18 22:57:46 +0530

    chore(cli): enable minify in bunup config

--- packages/cli/bunup.config.ts
+++ packages/cli/bunup.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "bunup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  minify: true,
+  banner: "#!/usr/bin/env node",
+  footer: "// Made by a human on earth!",
 });
