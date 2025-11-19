0d0effaab029739d6eb61518e8b1320f05d2a0b3
snelusha <hello@snelusha.dev>
2025-11-19 19:43:02 +0530

    feat(cli): add build time environment variables for version

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,12 @@
 import { createCli } from "trpc-cli";
 
-import { version } from "package";
+import { version as packageVersion } from "package";
 
 import { router } from "~/router";
 
 const args = process.argv.slice(2);
+
+const version = process.env.VERSION ?? packageVersion;
 
 if (args.includes("--version") || args.includes("-v")) {
   console.log(version);
