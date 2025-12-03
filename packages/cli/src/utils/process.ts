6cda751e37417cb92c4a1d933a91c2b10a854d5b
snelusha <hello@snelusha.dev>
2025-12-03 22:01:35 +0530

    feat(cli): display self update command instead of update message

--- packages/cli/src/utils/process.ts
+++ packages/cli/src/utils/process.ts
@@ -4,18 +4,13 @@ import dedent from "dedent";
 
 import { getAvailableUpdate } from "~/utils/update";
 
-import { getInstallationInfo } from "~/utils/installation-info";
-
 export async function displayUpdateNotice() {
   const update = await getAvailableUpdate();
-  if (update) {
-    const installationInfo = await getInstallationInfo();
-    const ins = installationInfo.updateMessage;
+  if (update)
     p.log.warn(
       dedent`A new version of noto is available: ${color.dim(update.current)} → ${color.green(update.latest)}
-      ${ins ?? ""}`.trim(),
+      Please run \`noto upgrade\` to update`.trim(),
     );
-  }
 }
 
 export const exit = async (code?: number, displayUpdate: boolean = true) => {
